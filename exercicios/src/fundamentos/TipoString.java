6f871cda1f3b18adf2c04724a12e5604bfd249b1
bbnsdevelop <bbnsdevelop@gmail.com>
2019-12-30 23:17:31 -0300

    algumas funcionalidade da class String

---
+++ exercicios/src/fundamentos/TipoString.java
@@ -0,0 +1,18 @@
+package fundamentos;
+
+public class TipoString {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Olá mundo".charAt(2));
+		
+		String s = " Boa tarde    ".trim();
+		System.out.println(s);
+		s = s.replaceAll("\\s", "");
+		System.out.println(s);
+		
+		System.out.println(s.toLowerCase().startsWith("boa"));
+		System.out.println(s.toLowerCase().endsWith("tarde"));
+		
+	}
+}
