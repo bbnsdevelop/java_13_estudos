7256baf6754aa97d862a9ae04a13de6e499e5230
bbnsdevelop <bbnsdevelop@gmail.com>
2019-12-30 23:27:14 -0300

    utilizando scanner para pegar informações digitadas pelo usuário

---
+++ exercicios/src/fundamentos/Console.java
@@ -0,0 +1,23 @@
+package fundamentos;
+
+import java.util.Scanner;
+
+public class Console {
+	
+	
+	public static void main(String[] args) {
+		System.out.println("Teste");
+		
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Digite algo: ");
+		String entrada = scan.nextLine();
+		
+		System.out.println(String.format("Algo digitado %s", entrada));
+		
+		scan.close();
+		
+	}
+
+}
