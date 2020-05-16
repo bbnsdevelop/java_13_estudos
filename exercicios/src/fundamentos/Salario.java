892c18006582dc90ffa105382dbaee5145c6032e
bbnsdevelop <bbnsdevelop@gmail.com>
2020-05-15 22:58:18 -0300

    somando tres salarios e dando uma media

---
+++ exercicios/src/fundamentos/Salario.java
@@ -0,0 +1,21 @@
+package fundamentos;
+
+import java.util.Scanner;
+
+public class Salario {
+
+	public static void main(String[] args) {
+
+		Double total = 0.0;
+		for (int i = 0; i <= 2; i++) {
+			System.out.println("Digite o salário " + (i + 1) + ": ");
+			Scanner scan = new Scanner(System.in);
+			String salario = scan.nextLine();
+			total += Double.valueOf(salario.replace(",", "."));
+		}
+		System.out.println("total de salario: " + total);
+		System.out.println("media de salario: " + (total / 3));
+
+	}
+
+}
