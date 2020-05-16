069ab94161a601bbfedeb346a3a5b1d9762d5ece
bbnsdevelop <bbnsdevelop@gmail.com>
2020-05-15 22:08:21 -0300

    converão de valores implícitos e explicitos

---
+++ exercicios/src/fundamentos/ConversaoStringNumero.java
@@ -0,0 +1,14 @@
+package fundamentos;
+
+import javax.swing.JOptionPane;
+
+public class ConversaoStringNumero {
+
+	public static void main(String[] args) {
+		String valor1 = JOptionPane.showInputDialog("Digite um número");
+		Integer i =  Integer.valueOf(valor1);
+		
+		JOptionPane.showMessageDialog(null, "Converteu para inteiro: " + i);
+	}
+
+}
