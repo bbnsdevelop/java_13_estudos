35579004e22a25491826073cde3fd0f22aba5197
bbnsdevelop <bbnsdevelop@gmail.com>
2020-05-25 22:24:03 -0300

    construtores sem dados de entrada, mas o default sendo inicializado

--- exercicios/src/br/com/poo/objetos/Data.java
+++ exercicios/src/br/com/poo/objetos/Data.java
@@ -5,11 +5,24 @@ public class Data {
 	public int dia;
 	public int mes;
 	public int ano;
+
+	public Data() {
+		this.dia = 1;
+		this.mes = 1;
+		this.ano = 1970;
+	}
+	
+	public Data(int dia, int mes, int ano) {
+		this.dia = dia;
+		this.mes = mes;
+		this.ano = ano;
+	}
 	
 	@Override
 	public String toString() {
 		return dia + "/" + mes + "/"  + ano ;
 	}
+
 	
 	
 
