265fed32ef14d2cb0c74ed5cdc19f973cdb8f9d8
bbnsdevelop <bbnsdevelop@gmail.com>
2020-06-01 22:14:21 -0300

    sobrescrevendo o equals e hashsetcode para não ter dados repitidos na collection

--- exercicios/src/br/com/poo/objetos/Produto.java
+++ exercicios/src/br/com/poo/objetos/Produto.java
@@ -32,4 +32,38 @@ public class Produto {
 		System.out.println(precoComDesconto);
 	}
 
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(preco);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (Double.doubleToLongBits(preco) != Double.doubleToLongBits(other.preco))
+			return false;
+		return true;
+	}
+	
+	
+
 }
