0865e8530fa69de67c45694160fad85eabd04b8a
bbnsdevelop <bbnsdevelop@gmail.com>
2020-06-14 19:37:16 -0300

    encapsulamento

---
+++ exercicios/src/br/com/poo/encapsulamento/casaB/Julia.java
@@ -0,0 +1,18 @@
+package br.com.poo.encapsulamento.casaB;
+
+import br.com.poo.encapsulamento.casaA.Ana;
+
+public class Julia {
+	
+	
+	void testeAcessos() {
+		Ana sogra = new Ana();
+		
+		System.out.println(sogra.todosSabem);
+		//System.out.println(sogra.formaDeFalar); 
+		// System.out.println(facoComida);
+		// System.out.println(esposa.segredo);	
+	}
+
+
+}
