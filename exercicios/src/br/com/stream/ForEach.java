5d2cbc3cb25d078fe2250030e5711fcb55b23e6f
bbnsdevelop <bbnsdevelop@gmail.com>
2020-06-15 20:03:24 -0300

    foreach lambda

---
+++ exercicios/src/br/com/stream/ForEach.java
@@ -0,0 +1,21 @@
+package br.com.stream;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ForEach {
+	
+	public static void main(String[] args) {
+		List<String> nomes = Arrays.asList("Bruno", "João", "Pedro","Maria");
+		
+		nomes.forEach(n ->{
+			String nome = n;
+			System.out.println("Nome: "+ nome);
+		});
+		
+		nomes.forEach(n ->System.out.println("Nome: " + n)); // Não precisa ter corpo quando tem apenas uma linha de código
+		
+		nomes.forEach(System.out::println); // Forma implicita 
+	}
+
+}
